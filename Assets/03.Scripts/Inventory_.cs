794aa7cd4dbe9f0d69460b6c13a460f8de050281
yslee523 <leeyoonsub523@gmail.com>
2022-06-16 17:26:25 +0900

    20220616 최종

---
+++ Assets/03.Scripts/Inventory_.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory_ : MonoBehaviour
+{
+    public Transform rootSlot;
+    public Store store;
+    private List<Slot> slots;
+
+    void Start()
+    {
+        slots = new List<Slot>();
+        int slotCnt = rootSlot.childCount;
+
+        for(int i = 0; i < slotCnt; i++)
+        {
+            var slot = rootSlot.GetChild(i).GetComponent<Slot>();
+
+            slots.Add(slot);
+        }
+
+        store.onSlotClick += BuyItem;
+    }
+
+    void BuyItem(ItemProperty item)
+    {
+        var emptySlot = slots.Find(t =>
+        {
+            return t.item == null || t.item.name == string.Empty;
+        });
+
+        if(emptySlot != null)
+        {
+            emptySlot.SetItem(item);
+        }
+    }
+}
---
+++ Assets/03.Scripts/Store.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Store : MonoBehaviour
+{
+    public ItemBuffer itemBuffer;
+    public Transform slotRoot;
+    private List<Slot> slots;
+    public System.Action<ItemProperty> onSlotClick; // 여기다가 외부 함수 연결
+    void Start()
+    {
+        slots = new List<Slot>();
+        int slotCnt = slotRoot.childCount;
+
+        for (int i = 0; i < slotCnt; i++)
+        {
+            var slot = slotRoot.GetChild(i).GetComponent<Slot>();
+
+            if(i < itemBuffer.items.Count)
+            {
+                slot.SetItem(itemBuffer.items[i]);
+            }
+            else
+            {
+                // 아이템 없는공간 버튼 안눌리게??@.@;;
+                slot.GetComponent<UnityEngine.UI.Button>().interactable = false;
+            }
+
+            slots.Add(slot);
+        }
+    }
+
+    void Update()
+    {
+    }
+
+    public void OnClickSlot(Slot slot)
+    {
+        if(onSlotClick != null)
+        {
+            onSlotClick(slot.item);
+        }
+    }
+}
